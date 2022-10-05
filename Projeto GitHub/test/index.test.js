24cada3000de1fecc7930c88345f0c4a1981614e
Caroliny França <francinha.caroliny@gmail.com>
2022-10-05 20:08:07 -0300

    API Github

---
+++ Projeto GitHub/test/index.test.js
@@ -0,0 +1,6 @@
+"use strict";
+describe('testing index', function () {
+    it('checking if everything its ok', () => {
+        expect(2 + 2).toBe(4);
+    });
+});
---
+++ Projeto GitHub/test/index.test.ts
@@ -0,0 +1,5 @@
+describe('testing index', function (){
+    it('checking if everything its ok', ()=>{
+        expect(2+2).toBe(4)
+    })
+})
